3d6472c68dd561bf7fd0783973f96701b63da9d1
eaphone <cyfdel@hotmail.com>
2026-03-06 22:26:16 +0800

    support TK024F3036

--- components/retro-go/targets/esp32-s3-devkit/config.h
+++ components/retro-go/targets/esp32-s3-devkit/config.h
@@ -23,42 +23,43 @@
 #define RG_SCREEN_ROTATE            0
 #define RG_SCREEN_VISIBLE_AREA      {0, 0, 0, 0}
 #define RG_SCREEN_SAFE_AREA         {0, 0, 0, 0}
-#define RG_SCREEN_INIT()                                                                                         \
-    ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
-    ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
-    ILI9341_CMD(0xE8, 0x85, 0x00, 0x78);                                                                         \
-    ILI9341_CMD(0xCB, 0x39, 0x2c, 0x00, 0x34, 0x02);                                                             \
-    ILI9341_CMD(0xF7, 0x20);                                                                                     \
-    ILI9341_CMD(0xEA, 0x00, 0x00);                                                                               \
-    ILI9341_CMD(0xC0, 0x1B);                 /* Power control   //VRH[5:0] */                                    \
-    ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
-    ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
-    ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
-    ILI9341_CMD(0x36, 0x68);                 /* Memory Access Control  (MX|MV|BGR) */                            \
-    ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
-    ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
-    ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
-    ILI9341_CMD(0xF2, 0x00);                 /* 3Gamma Function Disable */                                       \
-    ILI9341_CMD(0x26, 0x01);                 /* Gamma curve selected */                                          \
-    ILI9341_CMD(0xE0, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00); \
-    ILI9341_CMD(0xE1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F);
+#define RG_SCREEN_INIT()                                                                                            \
+    ILI9341_CMD(0x11, 0x00);                                                                                        \
+    rg_usleep(120 * 1000);                                                                                          \
+    ILI9341_CMD(0x36, 0x60);                                                                                        \
+    ILI9341_CMD(0x3A, 0x05);                                                                                        \
+    ILI9341_CMD(0xB2, 0x1F, 0x1F, 0x00, 0x33, 0x33);                                                                \
+    ILI9341_CMD(0xB7, 0x12);                                                                                        \
+    ILI9341_CMD(0xBB, 0x66);                                                                                        \
+    ILI9341_CMD(0xC0, 0x2c);                 /* Power control   //VRH[5:0] */                                       \
+    ILI9341_CMD(0xC2, 0x01);                 /* Power control   //SAP[2:0];BT[3:0] */                               \
+    ILI9341_CMD(0xC3, 0x15);           /* VCM control */                                                            \
+    ILI9341_CMD(0xC4, 0x20);                 /* VCM control2 */                                                     \
+    ILI9341_CMD(0xC6, 0x13);                 /* Memory Access Control  (MX|MV|BGR) */                               \
+    ILI9341_CMD(0xD0, 0xA4, 0xA1);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                        \
+    ILI9341_CMD(0xD6, 0xA1);           /* Display Function Control */                                               \
+    ILI9341_CMD(0xE0, 0xF0,  0x06,  0x0D,  0x0B,  0x0A,  0x07,  0x2E,  0x43,  0x45,  0x38,  0x14,  0x13,  0x25,  0x29);                                                                               \
+    ILI9341_CMD(0xE1, 0xF0,  0x07,  0x0A,  0x08,  0x07,  0x23,  0x2E,  0x33,  0x44,  0x3A,  0x16,  0x17,  0x26,  0x2C);                 /* 3Gamma Function Disable */                                       \
+    ILI9341_CMD(0x21, 0x00);                 /* Gamma curve selected */                                             \
+    ILI9341_CMD(0x29, 0x00);                                                                                        \
+    rg_usleep(50 * 1000);                                                                                           \
 
 
 // Input
 // Refer to rg_input.h to see all available RG_KEY_* and RG_GAMEPAD_*_MAP types
 #define RG_GAMEPAD_ADC_MAP {\
-    {RG_KEY_UP,    ADC_UNIT_1, ADC_CHANNEL_5, ADC_ATTEN_DB_11, 3072, 4096},\
-    {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 1024, 3071},\
-    {RG_KEY_DOWN,  ADC_UNIT_1, ADC_CHANNEL_5, ADC_ATTEN_DB_11, 1024, 3071},\
-    {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 3072, 4096},\
 }
 #define RG_GAMEPAD_GPIO_MAP {\
-    {RG_KEY_SELECT, .num = GPIO_NUM_16, .pullup = 1, .level = 0},\
-    {RG_KEY_START,  .num = GPIO_NUM_17, .pullup = 1, .level = 0},\
-    {RG_KEY_MENU,   .num = GPIO_NUM_18, .pullup = 1, .level = 0},\
-    {RG_KEY_OPTION, .num = GPIO_NUM_8,  .pullup = 1, .level = 0},\
-    {RG_KEY_A,      .num = GPIO_NUM_15, .pullup = 1, .level = 0},\
-    {RG_KEY_B,      .num = GPIO_NUM_5,  .pullup = 1, .level = 0},\
+    {RG_KEY_UP, .num = GPIO_NUM_11, .pullup = 1, .level = 0},\
+    {RG_KEY_DOWN, .num = GPIO_NUM_12, .pullup = 1, .level = 0},\
+    {RG_KEY_LEFT, .num = GPIO_NUM_13, .pullup = 1, .level = 0},\
+    {RG_KEY_RIGHT, .num = GPIO_NUM_14, .pullup = 1, .level = 0},\
+    {RG_KEY_START,  .num = GPIO_NUM_21, .pullup = 1, .level = 0},\
+    {RG_KEY_OPTION,  .num = GPIO_NUM_47, .pullup = 1, .level = 0},\
+    {RG_KEY_X,      .num = GPIO_NUM_41, .pullup = 1, .level = 0},\
+    {RG_KEY_Y,      .num = GPIO_NUM_42, .pullup = 1, .level = 0},\
+    {RG_KEY_A,      .num = GPIO_NUM_0, .pullup = 1, .level = 0},\
+    {RG_KEY_B,      .num = GPIO_NUM_48, .pullup = 1, .level = 0},\
 }
 
 // Battery
@@ -76,15 +77,15 @@
 #define RG_GPIO_LCD_MISO            GPIO_NUM_NC
 #define RG_GPIO_LCD_MOSI            GPIO_NUM_6
 #define RG_GPIO_LCD_CLK             GPIO_NUM_5
-#define RG_GPIO_LCD_CS              GPIO_NUM_8
+#define RG_GPIO_LCD_CS              GPIO_NUM_15
 #define RG_GPIO_LCD_DC              GPIO_NUM_7
-#define RG_GPIO_LCD_BCKL            GPIO_NUM_4
-#define RG_GPIO_LCD_RST             GPIO_NUM_38
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_NC
+#define RG_GPIO_LCD_RST             GPIO_NUM_2
 
-#define RG_GPIO_SDSPI_MISO          GPIO_NUM_26
-#define RG_GPIO_SDSPI_MOSI          GPIO_NUM_32
-#define RG_GPIO_SDSPI_CLK           GPIO_NUM_31
-#define RG_GPIO_SDSPI_CS            GPIO_NUM_33
+#define RG_GPIO_SDSPI_MISO          GPIO_NUM_45
+#define RG_GPIO_SDSPI_MOSI          GPIO_NUM_39
+#define RG_GPIO_SDSPI_CLK           GPIO_NUM_38
+#define RG_GPIO_SDSPI_CS            GPIO_NUM_40
 
 // External I2S DAC
 #define RG_GPIO_SND_I2S_BCK         41

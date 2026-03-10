49905007946091f429caa263e6b4856c0a8ff548
eaphone <cyfdel@hotmail.com>
2026-03-10 21:35:46 +0800

    fix battery state

--- components/retro-go/targets/esp32-s3-devkit/config.h
+++ components/retro-go/targets/esp32-s3-devkit/config.h
@@ -97,14 +97,11 @@
     {RG_KEY_B,      .num = GPIO_NUM_48, .pullup = 1, .level = 0},\
 }
 
-
-/****************************************************************************
- * Battery                                                                  *
- ****************************************************************************/
-#define RG_BATTERY_DRIVER           1
-#define RG_BATTERY_ADC_UNIT         ADC_UNIT_1
-#define RG_BATTERY_ADC_CHANNEL      ADC_CHANNEL_3
-#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3500.f) / (4200.f - 3500.f) * 100.f)
+// Battery
+#define RG_BATTERY_DRIVER           1  
+#define RG_BATTERY_ADC_UNIT         ADC_UNIT_2
+#define RG_BATTERY_ADC_CHANNEL      ADC_CHANNEL_5
+#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 2500.f) / (4200.f - 2500.f) * 100.f)
 #define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
 
 

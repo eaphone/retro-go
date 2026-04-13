8dd768b61d73dd2754ed925a06dac1e06e1780ce
eaphone <cyfdel@hotmail.com>
2026-04-14 00:33:36 +0800

    temp fix gba save state

--- components/retro-go/rg_system.c
+++ components/retro-go/rg_system.c
@@ -269,7 +269,7 @@ static void system_monitor_task(void *arg)
         update_indicators(false);
 
         // Try to avoid complex conversions that could allocate, prefer rounding/ceiling if necessary.
-        rg_system_log(RG_LOG_DEBUG, NULL, "STACK:%d, HEAP:%d+%d (%d+%d), BUSY:%d%%, FPS:%d (S:%d R:%d+%d), BATT:%d",
+        rg_system_log(RG_LOG_VERBOSE, NULL, "STACK:%d, HEAP:%d+%d (%d+%d), BUSY:%d%%, FPS:%d (S:%d R:%d+%d), BAT:%d",
             statistics.freeStack[0],
             statistics.freeMemoryInt / 1024,
             statistics.freeMemoryExt / 1024,

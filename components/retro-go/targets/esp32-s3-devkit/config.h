330e08d3606973a1049e2587152d9aaa392508bb
eaphone <cyfdel@hotmail.com>
2026-04-15 21:16:22 +0800

    optimize battery calculation

--- components/retro-go/targets/esp32-s3-devkit/config.h
+++ components/retro-go/targets/esp32-s3-devkit/config.h
@@ -106,7 +106,7 @@
 #define RG_BATTERY_DRIVER           1  
 #define RG_BATTERY_ADC_UNIT         ADC_UNIT_2
 #define RG_BATTERY_ADC_CHANNEL      ADC_CHANNEL_5
-#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3200.f) / (4000.f - 3200.f) * 100.f)
+#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3200.f) / (3900.f - 3200.f) * 100.f)
 #define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
 
 /****************************************************************************
